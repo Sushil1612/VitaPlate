6a2e5a49b39d7231bd73580f888ac0e3928721d8
shantanu <shantanukothavale1@gmail.com>
2025-06-07 16:26:30 +0530

    Initial commit

---
+++ src/test/java/com/cdac/vitaplate/VitaplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdac.vitaplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitaplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
